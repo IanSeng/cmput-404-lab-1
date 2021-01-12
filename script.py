8bfb4cef63d3338b5e13435197c4ffa12ab1987d
Ian Seng <ianseng@outlook.com>
2021-01-11 18:22:11 -0700

    complete question 8

--- script.py
+++ script.py
@@ -1,7 +1,22 @@
 import pkg_resources
 import requests
 
+# google URL 
+googleURL = 'https://google.com'
+# github URL 
+githubURL = 'https://raw.githubusercontent.com/IanSeng/cmput-404-lab-1/master/script.py'
+# github output file name 
+githubFileName = 'githubOutput.txt'
+
+# Print package requests
 print(pkg_resources.get_distribution("requests").version)
 
-r = requests.get('https://google.com')
-print(r.text)
+# Get and print Google home page
+googleResp= requests.get(googleURL)
+print(googleResp.text)
+
+# Get and print github script home page
+githubResp = requests.get(url)
+f = open('githubFileName', 'w')
+f.write(githubResp.text)
+f.close()
